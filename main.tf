c879fc7e89b740362df0f3b1f77e232a53dccb8a
shoudao867 <159771758+te578@users.noreply.github.com>
2026-05-30 00:17:56 +0900

    initial terraform build aws

---
+++ main.tf
@@ -0,0 +1,48 @@
+
+#下記設定関係のため、変更禁止
+#================================================================================================
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-shimasan-20260529"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "my-vpc"
+    }
+}
+
+resource  "aws_s3_bucket" "tfstate" {
+  bucket = "tfstate-shimasan-20260529"  # 名前は自由に
+}
+
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+#==================================================================================================
+
+
+
+
+
+
+
+
+
+
