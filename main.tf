e30f97d91cdd91f1385fa7355bc136037c0b3072
shoudao867 <159771758+te578@users.noreply.github.com>
2026-06-10 22:04:27 +0900

    private subnet add

--- main.tf
+++ main.tf
@@ -44,10 +44,44 @@ resource "aws_subnet" "my_subnet" {
     availability_zone = "ap-northeast-1d"
     map_public_ip_on_launch = true
     tags = {
-        Name = "my-subnet"
+        Name = "public-subnet-1"
     }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 
+  tags = {
+    Name = "my-igw"
+  }
+}
 
+# ルートテーブル
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# サブネットとルートテーブルの関連付け
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_subnet" "my_subnet2" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-northeast-1d"
+
+    tags = {
+        Name = "private-subnet-1"
+    }
+}
